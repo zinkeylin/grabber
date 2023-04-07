31902fe0cf595fe0abd11ddd77b78a9e4952f631
zinkeylin <130042413+zinkeylin@users.noreply.github.com>
2023-04-07 16:35:48 +0300

    Update main.go
    многопоточный граббер на основе горутины-анонимной функции

--- main.go
+++ main.go
@@ -1,69 +1,159 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 const empty = ""
 
-func main() {
-
-	// Парсинг флагов
-	src := flag.String("src", empty, "path to URL")
-	dest := flag.String("dest", empty, "path to output directory")
+// Парсинг флагов
+func parseFlags() (string, string, error) {
+	flagSrc := flag.String("src", empty, "path to URL")
+	flagDest := flag.String("dest", empty, "path to output directory")
 	flag.Parse()
-	if *src == empty {
-		fmt.Println("missing src flag")
-		os.Exit(1)
+	src, dest := *flagSrc, *flagDest
+	if src == empty {
+		return empty, empty, errors.New("missing src flag")
 	}
-	if *dest == empty {
-		fmt.Println("missing dest flag")
-		os.Exit(1)
+	if dest == empty {
+		return empty, empty, errors.New("missing dest flag")
 	}
-	// Чтение файла
-	bs, err := ioutil.ReadFile(*src)
+	return src, dest, nil
+}
+
+// Чтение файла
+func readFile(src string) (string, error) {
+	bs, err := ioutil.ReadFile(src)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return empty, err
 	}
-	// Открытие директории
-	outputDir, err := os.Open(*dest)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer outputDir.Close()
-	// Парсинг файла (срезом)
-	urls := strings.Split(string(bs), "\n")
-	// Цикл по срезу
+	return string(bs), nil
+}
+
+// Парсинг ссылок (срезом)
+func parseURLs(rawURLs string) []string {
+	urls := strings.Split(string(rawURLs), "\n")
 	for i, url := range urls {
 		if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
-			url = "http://" + url
+			urls[i] = "http://" + url
 		}
-		// HTTP-запрос
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println(url, "- not ok")
-			continue
-		}
-		defer resp.Body.Close()
-		// Создание файла
-		out, err := os.Create(outputDir.Name() + "/" + strconv.Itoa(i) + ".html")
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer out.Close()
-		// Запись в файл
-		io.Copy(out, resp.Body)
-		fmt.Println(url, "- ok")
 	}
+	return urls
+}
+
+// проверка директории (возвращает её путь, если она существует)
+func checkDir(dest string) (string, error) {
+	outputDir, err := os.Open(dest)
+	defer outputDir.Close()
+	if err != nil {
+		return empty, err
+	}
+	return outputDir.Name(), nil
+}
+
+// логирование результата
+func log(url string, status bool) {
+	if status {
+		fmt.Printf("%s - ok\n", url)
+	} else {
+		fmt.Printf("%s - not ok\n", url)
+	}
+}
+
+// HTTP-запрос
+func getBody(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log(url, false)
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
+}
+
+// Создание файла
+func makeHTMLFile(name, path string) (*os.File, error) {
+	file, err := os.Create(path + "/" + name + ".html")
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+// Запись body в файл
+func writeFile(file *os.File, body []byte) error {
+	_, err := file.Write(body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+
+	src, dest, err := parseFlags()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	path, err := checkDir(dest)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	rawURLs, err := readFile(src)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	urls := parseURLs(rawURLs)
+
+	var wg sync.WaitGroup
+	// Цикл по срезу
+	for i, url := range urls {
+		wg.Add(1)
+		go func(i int, url string) {
+			defer wg.Done()
+		
+			body, err := getBody(url)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			out, err := makeHTMLFile(strconv.Itoa(i), path)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			err = writeFile(out, body)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			log(url, true)
+		
+		}(i, url)
+
+	}
+
+	wg.Wait()
+
 }
